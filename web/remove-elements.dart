bfa8dba43ead82c9d6dff8e14e7584f61e73d34e
naivvve <1532619822@qq.com>
2022-06-07 19:47:36 +0800

    Message

--- web/remove-elements.dart
+++ web/remove-elements.dart
@@ -2,18 +2,18 @@
 
 import 'dart:html';
 
-final InputElement toDoInput = querySelector('#to-do-input') as InputElement;
-final UListElement toDoList = querySelector('#to-do-list') as UListElement;
+final InputElement MenuInput = querySelector('#Menu-input') as InputElement;
+final UListElement MenuList = querySelector('#Menu-list') as UListElement;
 final ButtonElement deleteAll = querySelector('#delete-all') as ButtonElement;
 
 void main() {
-  toDoInput.onChange.listen(addToDoItem);
-  deleteAll.onClick.listen((_) => toDoList.children.clear());
+  MenuInput.onChange.listen(addToDoItem);
+  deleteAll.onClick.listen((_) => MenuList.children.clear());
 }
 
 void addToDoItem(Event e) {
-  final newToDo = LIElement()..text = toDoInput.value;
-  newToDo.onClick.listen((_) => newToDo.remove());
-  toDoInput.value = '';
-  toDoList.children.add(newToDo);
+  final newMenu = LIElement()..text = MenuInput.value;
+  newMenu.onClick.listen((_) => newMenu.remove());
+  MenuInput.value = '';
+  MenuList.children.add(newMenu);
 }
